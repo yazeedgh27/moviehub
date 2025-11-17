16f01f1ea1fdb766ef7c23d79a976660c4a665e6
yazeedgh27 <yazeed.gh27@gmail.com>
2025-11-17 18:36:21 +0300

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,11 @@
+function validateForm() {
+    let name = document.getElementById("name").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let message = document.getElementById("message").value.trim();
+
+    if (name === "" || email === "" || message === "") {
+        alert("Please fill in all fields.");
+        return false;
+    }
+    return true;
+}
